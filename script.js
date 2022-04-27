7b10738155ce1a137f2bd6263679d44675de2fd8
Claudia Agulhas <103274417+ClaudiaAgulhas@users.noreply.github.com>
2022-04-27 19:09:15 +0200

    landing page

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("image").addEventListener("click", (event) => {
+  event.target.setAttribute(
+    "src",
+    "https://cdns-images.dzcdn.net/images/artist/69c569506a8ff6ab0edfecbd1adf94b0/500x500.jpg"
+  );
+});
